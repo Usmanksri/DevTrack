3e7fbb906b8fdf6ef5cec5b2635577e26164339b
Usmanksri <usmankasuri123@gmail.com>
2023-05-22 16:58:50 +0500

    added for test

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,8 +2,7 @@ class TasksController < ApplicationController
 
     before_action :set_task, only: [:show, :edit, :update, :destroy]
     include ApplicationHelper
-
-
+    
     def index
         @tasks=Task.all
     end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,14 @@ Rails.application.routes.draw do
   root "pages#home"
   resources :projects
   #  Add route to select a project
-  resources :tasks
+  post 'select_project', to: 'projects#select_project', as: 'select_project'
+
+
+  resources :tasks do
+    post 'comments', to: 'comments#create'
+  end
+  
+  
 
 
 end
